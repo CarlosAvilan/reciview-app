76c71e40433fb502cffdb96b7446b06725f92a47
Carlos Avilán <cdavilan@gmail.com>
2026-05-26 18:26:35 -0300

    Se agregó un mensaje de éxito luego deguardar un gasto manual

--- app/src/main/java/ar/edu/uade/capturarecibosapp/ui/screens/ManualExpenseScreen.kt
+++ app/src/main/java/ar/edu/uade/capturarecibosapp/ui/screens/ManualExpenseScreen.kt
@@ -1,8 +1,10 @@
 package ar.edu.uade.capturarecibosapp.ui.screens
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -10,13 +12,19 @@ import ar.edu.uade.capturarecibosapp.ui.components.ExpenseForm
 import ar.edu.uade.capturarecibosapp.ui.components.TopBar
 import ar.edu.uade.capturarecibosapp.ui.theme.ReciViewTheme
 import ar.edu.uade.capturarecibosapp.ui.viewmodel.ManualExpenseViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun ManualExpenseScreen(viewModel: ManualExpenseViewModel, onBackClick: () -> Unit) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
     Scaffold(
         topBar = {
             TopBar(title = "Cargar Gasto Manual", onBackClick = onBackClick)
         },
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         containerColor = Color(0xFFF5F5F5)
     ) { paddingValues ->
         ExpenseForm(
@@ -32,7 +40,16 @@ fun ManualExpenseScreen(viewModel: ManualExpenseViewModel, onBackClick: () -> Un
             buttonText = "Guardar Gasto",
             onButtonClick = { 
                 viewModel.guardarGasto()
-                onBackClick() // Navegamos de vuelta tras guardar
+                scope.launch {
+                    // Mostramos el mensaje de confirmación
+                    snackbarHostState.showSnackbar(
+                        message = "Se ha guardado el nuevo gasto",
+                        duration = SnackbarDuration.Short
+                    )
+                    // Esperamos 1 segundo para que el usuario lo lea antes de volver
+                    delay(1000)
+                    onBackClick() 
+                }
             }
         )
     }
